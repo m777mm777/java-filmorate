33a9fa441f67d70879524c7d3d1865398e516e97
Iurii <vans.inc@yandex.ru>
2024-01-06 05:05:21 +0300

    ФЗ10 fix3

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -23,13 +23,16 @@ public class FilmService {
     }
 
     public void addLike(Long idFilm, Long idUser) {
+
         if(checkIdFilm(idFilm) & checkIdUser(idUser)) {
             filmStorage.addLike(idFilm, idUser);
         }
     }
 
     public void removeLike(Long idFilm, Long idUser) {
+        
         if(checkIdFilm(idFilm) & checkIdUser(idUser)) {
+
             filmStorage.removeLike(idFilm, idUser);
         }
     }
@@ -64,6 +67,7 @@ public class FilmService {
 
     private Boolean checkIdUser(Long id) {
         Long i = Optional.of(id).orElseThrow(() -> new DataNotFoundException("Не верный id пользователя"));
+
         if(userStorage.getById(i) == null) {
             throw new DataNotFoundException("Такого пользователя нет");
         }
@@ -72,6 +76,7 @@ public class FilmService {
 
     private Boolean checkIdFilm(Long id) {
         Long i = Optional.of(id).orElseThrow(() -> new DataNotFoundException("Не верный id фильма"));
+
         if(filmStorage.getById(i) == null) {
             throw new DataNotFoundException("Такого фильма нет");
         }
